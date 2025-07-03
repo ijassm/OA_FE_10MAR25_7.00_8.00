39428d81ab811e21b7e60fb360dd1d6842612f51
ijassm <ijass.ocean@gmail.com>
2025-07-03 20:10:20 +0530

    Add images to menu items and enhance layout for better responsiveness

--- JS/project/data.js
+++ JS/project/data.js
@@ -4,47 +4,55 @@ export const menuItems = [
         description: "Fluffy pancakes served with maple syrup and butter.",
         price: 120,
         type: "breakfast",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Masala-Dosa.jpg"
     },
     {
         title: "Masala Dosa",
         description: "Crispy South Indian dosa stuffed with spiced potato filling.",
         price: 90,
         type: "breakfast",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Bisi-Bele-Bath-1024x538.jpg"
     },
     {
         title: "Chicken Biryani",
         description: "Aromatic rice dish cooked with marinated chicken and spices.",
         price: 180,
         type: "lunch",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Mangalorean-Fish-Curry-1024x538.jpg"
     },
     {
         title: "Veg Thali",
         description: "Traditional Indian thali with roti, dal, sabzi, rice and dessert.",
         price: 150,
         type: "lunch",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Akki-Roti-1024x538.jpg"
     },
     {
         title: "Oreo Shake",
         description: "Chilled milkshake blended with Oreo cookies and vanilla ice cream.",
         price: 100,
         type: "shakes",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Rava-Idli-1024x538.jpg"
     },
     {
         title: "Mango Smoothie",
         description: "Refreshing mango smoothie with yogurt and honey.",
         price: 90,
         type: "shakes",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Ragi-Mudde-1024x538.jpg"
     },
     {
         title: "Butter Chicken",
         description: "Tender chicken cooked in a creamy tomato-based gravy.",
         price: 200,
         type: "dinner",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Butter-Chicken.jpg"
     },
     {
         title: "Palak Paneer",
         description: "Paneer cubes simmered in creamy spinach gravy.",
         price: 160,
         type: "dinner",
+        image: "https://blog.swiggy.com/wp-content/uploads/2024/02/Mysore-Pak-1024x538.jpg"
     }
 ];
--- JS/project/index.js
+++ JS/project/index.js
@@ -7,63 +7,58 @@ const renderUi = (data) => {
     menuContainer.innerHTML = data;
 };
 
-
 function filterData(type) {
     console.log("called", type);
 
     switch (type) {
-        case "all":
-            {
-                const cardUIData = menuItems.map((data) => card(data)).join("")
-                renderUi(cardUIData);
-                break;
-            }
-        case "breakfast":
-            {
-                const breakfastData = menuItems.filter((data) => data.type === type);
-                const cardUIData = breakfastData.map((data) => card(data)).join("");
-                renderUi(cardUIData);
-                break;
-            }
-        case "lunch":
-            {
-                const breakfastData = menuItems.filter((data) => data.type === type);
-                const cardUIData = breakfastData.map((data) => card(data)).join("");
-                renderUi(cardUIData);
-                break;
-            }
-        case "shakes":
-            {
-                const breakfastData = menuItems.filter((data) => data.type === type);
-                const cardUIData = breakfastData.map((data) => card(data)).join("");
-                renderUi(cardUIData);
-                break;
-            }
-        case "dinner":
-            {
-                const breakfastData = menuItems.filter((data) => data.type === type);
-                const cardUIData = breakfastData.map((data) => card(data)).join("");
-                renderUi(cardUIData);
-                break;
-            }
+        case "all": {
+            const cardUIData = menuItems.map((data) => card(data)).join("");
+            renderUi(cardUIData);
+            break;
+        }
+        case "breakfast": {
+            const breakfastData = menuItems.filter((data) => data.type === type);
+            const cardUIData = breakfastData.map((data) => card(data)).join("");
+            renderUi(cardUIData);
+            break;
+        }
+        case "lunch": {
+            const lunchData = menuItems.filter((data) => data.type === type);
+            const cardUIData = lunchData.map((data) => card(data)).join("");
+            renderUi(cardUIData);
+            break;
+        }
+        case "shakes": {
+            const ShakeData = menuItems.filter((data) => data.type === type);
+            const cardUIData = ShakeData.map((data) => card(data)).join("");
+            renderUi(cardUIData);
+            break;
+        }
+        case "dinner": {
+            const DinnerData = menuItems.filter((data) => data.type === type);
+            const cardUIData = DinnerData.map((data) => card(data)).join("");
+            renderUi(cardUIData);
+            break;
+        }
 
         default:
             break;
     }
-};
+}
 
-function card({ title, description, price }) {
+function card({ title, description, price, image }) {
     return `
-        <div class="flex-item col-6 d-flex gap-4 p-4">
-          <div>
+        <div class="flex-item col-12 col-lg-6 d-md-flex gap-4 p-md-2">
+          <div class="image-container">
             <img
               class="w-100 image"
-              src="https://static.toiimg.com/thumb/msid-112019658,width-1280,height-720,resizemode-4/112019658.jpg"
-              alt="ice cream"
+              src=${image}
+              alt=${image}
+              width="100%"
             />
           </div>
 
-          <article class="content">
+          <article class="content flex-grow-2 my-4">
             <h4 class="d-flex justify-content-between">
               <span>${title}</span> <span>$${price}</span>
             </h4>
@@ -75,11 +70,17 @@ function card({ title, description, price }) {
      `;
 }
 
+const cardUIData = menuItems.map((data) => card(data)).join("");
 
-const cardUIData = menuItems.map((data) => card(data)).join("")
-
-renderUi(cardUIData)
-
+renderUi(cardUIData);
 
 // Make it available to inline onclick handlers
 window.filterData = filterData;
+
+fetch(
+    "https://dummyjson.com/products"
+)
+    .then((data) => data.json())
+    .then((data) => {
+        console.log(data);
+    });
